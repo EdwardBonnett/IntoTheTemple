79a9e1ed8580299d329252319b1a5073b529d6c4
Edward Bonnett <edwardbonnett@gmail.com>
2016-09-10 18:19:01 +0100

    Add files via upload

---
+++ Controllers/RoomController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using TextAdventure.Models;
+using Microsoft.AspNetCore.Hosting;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Routing;
+
+namespace TextAdventure.Controllers
+{
+    public class RoomController : Controller
+    {
+        private readonly IHostingEnvironment _hostEnvironment;
+        public RoomController(IHostingEnvironment hostEnvironment)
+        {
+            _hostEnvironment = hostEnvironment;
+        }
+
+        [Route("{id?}")]
+        public IActionResult Index(int? id)
+        {
+            id = id ?? 1;
+            Room room = loadRoom(id.Value);
+            return View(room);
+        }
+
+        [Route("/api/Room/{id}")]
+        public string Room(int id)
+        {
+            return System.IO.File.ReadAllText(_hostEnvironment.WebRootPath + $"\\rooms\\{id}.json");
+        }
+
+        private Room loadRoom(int id)
+        {
+            var lines = System.IO.File.ReadAllText(_hostEnvironment.WebRootPath + $"\\rooms\\{id}.json");
+            Room room = JsonConvert.DeserializeObject<Room>(lines);
+            return room;
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Room.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TextAdventure.Models
+{
+    public class Room
+    {
+        [JsonProperty("l")]
+        public string LastAction { get; set; }
+
+        [JsonProperty("r")]
+        public string RoomText { get; set; }
+
+        [JsonProperty("o")]
+        public List<OptionsText> Options { get; set; }
+
+        [JsonProperty("c")]
+        public string Class { get; set; }
+
+        [JsonProperty("f")]
+        public string Font { get; set; }
+    }
+
+    public class OptionsText
+    {
+        [JsonProperty("i")]
+        public int Id { get; set; }
+
+        [JsonProperty("t")]
+        public string Text { get; set; }
+    }
+}
